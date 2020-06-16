1e602846e6bbe9e3671931ba4efe6dacb7b440ae
Olaf <67029706+0lafs@users.noreply.github.com>
2020-06-16 16:35:36 -0700

    created html and css file.
    first version of my website.

---
+++ index.cxx
@@ -0,0 +1,27 @@
+p{
+ color: blue;
+ background-color: green;
+
+}
+h2 {
+    color: purple;
+    tex-decoration: underline;
+    font-family: Ariel
+
+
+
+}
+
+h1 {
+    color: blue;
+    font-family: 'Walter Turncoat';
+
+}
+
+
+div{
+    background-color: red;
+    padding: 0;
+    border: 1px solod black
+    margine: 0;
+}
