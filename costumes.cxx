ffc573fd769f5cbbc33da0c9d14d5ee0652d0cb2
Olaf <67029706+0lafs@users.noreply.github.com>
2020-06-30 16:08:30 -0700

    Updated Code

--- costumes.cxx
+++ costumes.cxx
@@ -1,27 +1,85 @@
+@import url('https://fonts.googleapis.com/css?family=Work+Sans:400,600');
+
+  
+
+
+
+header {
+    background: #239c35;
+     }
+
+.container {
+        width: 70%;             
+        margin: 0 auto; }
+    
+
+  
+body {
+	margin: 0;
+	background: rgb(94, 82, 68);
+	font-family: 'Work Sans', sans-serif;
+    font-weight: 800; }
+    
+
+      
+h1{
+    color: blanchedalmond;
+    font-family: 'Walter Turncoat';
+}
+
 p{
- color: blue;
- background-color: green;
+ color: rgb(0, 0, 0);
 
 }
 h2 {
     color: purple;
-    tex-decoration: underline;
+    text-decoration: underline;
     font-family: Ariel
 
-
-
-}
-
-h1 {
-    color: blue;
-    font-family: 'Walter Turncoat';
-
 }
 
 
-div{
-    background-color: red;
-    padding: 0;
-    border: 1px solod black
-    margine: 0;
-}
+
+
+
+
+.logo {
+    float: left;
+    padding: 0 px ;
+    width: 10%;
+	margin: 0 auto;
+ }
+
+ nav {
+    float: right;
+  }
+  
+  nav ul {
+    margin: 0;
+    padding: 10;
+    list-style: none;
+  }
+  
+  nav li {
+    display: inline-block;
+    margin-left: 70px;
+    padding-top: 23px;
+  
+    position: relative;
+  }
+  
+  nav a {
+    color: #444;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 14px;
+  }
+  
+  nav a:hover {
+    color: #000;
+  }
+  
+    
+          
+
+
